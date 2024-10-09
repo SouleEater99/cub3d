648a66f2560416c5474ce7cfa8c5c1ebd4cf9cf6
souleeater <souleeater@kali.kali>
2024-10-09 05:16:41 +0100

    => creating Makefile

---
+++ Makefile
@@ -0,0 +1,36 @@
+NAME = cub3d
+RM = rm -rf
+CC = cc  
+CFLAG = -Wall -Wextra -Werror -fsanitize=address -g3
+MLX_FLAG = -lX11 -lm -lz -lXext
+LIBFT = include/lib_ft/libft.a
+MLX_LIB =	./include/minilibx/minilibx-linux/libmlx.a \
+			./include/minilibx/minilibx-linux/libmlx_Linux.a
+SRC = ./src/cub3d.c\
+
+OBJ = $(SRC:.c=.o)
+
+%.o: %.c
+	$(CC) $(CFLAG) -c $< -o $@
+
+all : $(NAME)
+
+$(NAME) : $(OBJ) $(MLX_LIB) $(LIBFT)
+	$(CC) $(CFLAG) $(OBJ) $(LIBFT) $(MLX_LIB) $(MLX_FLAG) -o $(NAME)
+
+$(LIBFT):
+	@make -sC ./include/lib_ft all
+
+$(MLX_LIB):
+	@make -sC ./include/minilibx/minilibx-linux
+
+clean :
+	$(RM) $(OBJ)
+	@make -sC ./include/lib_ft clean
+	@make -sC ./include/minilibx/minilibx-linux clean
+
+fclean : clean
+	$(RM) $(NAME)
+	@make -sC ./include/lib_ft fclean
+
+re : fclean all
---
+++ include/cub3d.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maim <ael-maim@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 04:00:30 by ael-maim          #+#    #+#             */
+/*   Updated: 2024/10/09 04:02:09 by ael-maim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "./minilibx/minilibx-linux/mlx.h"
+#include "./minilibx/minilibx-linux/mlx_int.h"
+
+
+
+
+#endif
