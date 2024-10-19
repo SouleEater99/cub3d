070ab94188551b080e28bc2df54bd240e4476c36
Aziz018 <khatabiaziz707@gmail.com>
2024-10-19 08:40:22 +0100

    support macOS and linux in Makefile

--- mandatory/includes/cub3d.h
+++ mandatory/includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aziz <aziz@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 13:04:03 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/10/16 19:41:44 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/10/19 08:39:58 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,14 @@
 
 # define TILE_SIZE      32      // the cell grid size
 
-# define MOVE_SPEED     0.08     // player speed
-# define ROT_SPEED      0.04     // Rotation speed (in radians)
-// # define MOVE_SPEED     0.01     // player speed
-// # define ROT_SPEED      0.006     // Rotation speed (in radians)
+// for mac
+// # define MOVE_SPEED     0.08     // player speed
+// # define ROT_SPEED      0.04     // Rotation speed (in radians)
+
+// for linux
+
+# define MOVE_SPEED     0.02     // player speed
+# define ROT_SPEED      0.01     // Rotation speed (in radians)
 
 # define MAP_WIDTH      9       // just an example
 # define MAP_HEIGHT     9       // just an example
