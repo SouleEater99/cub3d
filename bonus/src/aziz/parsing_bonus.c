9914029e05d21002f17c17467280dddfb19dac35
Aziz018 <khatabiaziz707@gmail.com>
2024-12-24 09:32:00 +0100

    smaaaaall error in bonus parsing

--- bonus/src/aziz/parsing_bonus.c
+++ bonus/src/aziz/parsing_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:57:39 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/12/04 14:19:38 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/12/24 09:30:11 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ int	parse_map(t_data *data, int ac, char **av)
 	{
 		print_error("Error: bad arguments!\n", __FILE__, __LINE__);
 		printf("Usage: ./cub3d <map_path>\n");
-		return (1);
+		return (0);
 	}
 	ft_memset(&data->map, 0, sizeof(t_map));
 	if (!check_extension(av[1], ".cub"))
